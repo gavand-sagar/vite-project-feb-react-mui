a67e689cb124440de9774120f6588bad5984000e
gavand-sagar <gavand.sagar.s@gmail.com>
2026-02-26 14:15:23 +0530

    lab setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Person } from './Sagar'
-import JsxRules from './JsxRules'
-import Panel from './Panel'
-import ListRendering from './ListRendering'
-import ListRenderingV2 from './ListRenderingV2'
-import ListRenderingV3 from './ListRenderingV3'
-import UseEffectDemos from './UseEffectDemos'
-import UseEffectForCleanup from './UseEffectForCleanup'
+import Lab1 from './HandleInput/Lab1'
 
 
 function App() {
-  const [flag, setFlag] = useState(false)
   return (
     <>
-      <button onClick={() => setFlag(!flag)}>Load</button>
-      <hr />
-      {/* <button className='my-btn'>hello</button> */}
-      {/* <Panel /> */}
-      {/* <UseEffectDemos/> */}
-      {/* <UseEffectForCleanup/> */}
-      {
-        flag && <UseEffectForCleanup/>
-      }
+      <Lab1/>
     </>
   )
 }
---
+++ src/HandleInput/Lab1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Lab1({}: Props) {
+  return (
+    <div>Lab1</div>
+  )
+}
