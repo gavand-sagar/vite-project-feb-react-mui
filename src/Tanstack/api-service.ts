cd3555847702278552b69e022bc312c8be5af6cf
gavand-sagar <gavand.sagar.s@gmail.com>
2026-03-03 16:06:00 +0530

    proxy added in react app

--- src/Tanstack/api-service.ts
+++ src/Tanstack/api-service.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export function getUserWithUserIdParam({ queryKey }: { queryKey: any[] }) {
     // const param = queryKey[1]
     const [param] = queryKey
-    return axios.get('https://dummyjson.com/users/' + param)
+    return axios.get('/api/users/' + param)
         .then(x => x.data)
 }
