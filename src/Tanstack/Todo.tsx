8cae00ad49b565bcf166c8c55d5498f977c61200
gavand-sagar <gavand.sagar.s@gmail.com>
2026-03-03 17:29:13 +0530

    tanstack post req

--- src/Tanstack/Todo.tsx
+++ src/Tanstack/Todo.tsx
@@ -1,9 +1,16 @@
 import { CircularProgress } from '@mui/material'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 type Props = {}
+
+type TodoType = {
+    title: string;
+    description: string;
+    dueDate: string
+}
 
 export default function Todo({ }: Props) {
     // const [todos, setTodos] = useState<any>(null)
@@ -14,14 +21,49 @@ export default function Todo({ }: Props) {
     //         })
     // }, [])
 
-    const { data: todos, isFetching } = useQuery({
+    const queryClient = useQueryClient();
+
+    const { handleSubmit, register } = useForm<TodoType>()
+    const { data: todos, isFetching, refetch } = useQuery({
         queryKey: ['todos'],
         queryFn: () => axios.get("/api/todos")
             .then(x => x.data)
     })
 
+    function addTodo({ title, description, dueDate }: TodoType) {
+        return axios.post('/api/todos', { title, description, dueDate })
+    }
+
+    const createTodoMutation = useMutation({
+        mutationFn: addTodo,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['todos'] })
+        }
+    })
+
+    function mySubmit(data: TodoType) {
+        createTodoMutation.mutate(data)
+    }
+
     return (
         <div>
+
+            <form onSubmit={handleSubmit(mySubmit)}>
+                <div>
+                    <input placeholder='title' {...register('title')} />
+                </div>
+                <div>
+                    <input placeholder='description' {...register('description')} />
+                </div>
+                <div>
+                    <input placeholder='dueDate' {...register('dueDate')} type='date' />
+                </div>
+                <button type='submit'>Save Todo</button>
+            </form>
+
+            <button onClick={() => refetch()}>Refetch</button>
+
+            <hr />
             {
                 isFetching ? <CircularProgress />
                     :
