dd849634aa4fb5244f1dc4768d5943a21ffce811
gavand-sagar <gavand.sagar.s@gmail.com>
2026-03-04 15:44:41 +0530

    useMemo() and react.memo()

--- src/DemoForMemo/DemoForMemo.tsx
+++ src/DemoForMemo/DemoForMemo.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react"
+import React, { memo, useEffect, useMemo, useState } from "react"
 
 type Props = {}
 
 export function DemoForMemoChild({ }: Props) {
+    const [childState, setChildState] = useState(0)
     console.log("Child Rerendered")
     let v = 0;
     for (let i = 0; i < 1000; i++) {
@@ -10,22 +11,33 @@ export function DemoForMemoChild({ }: Props) {
     }
     console.log("Child Calculated " + v + " times")
     return (
-        <div>DemoForMemo {v}</div>
+        <div>
+            DemoForMemo {v}
+            <h1>Child {childState} <button onClick={() => setChildState(childState + 1)}>INC</button></h1>
+
+        </div>
     )
 }
 
-const BetterChild = React.memo(DemoForMemoChild)
+const MemoizedChild = memo(DemoForMemoChild)
 
 type ParentProps = {}
 
 export default function DemoForMemo({ }: ParentProps) {
-    const [ParentState, setParentState] = useState(0)
-    console.log("Parent Re-rendered")
+    const [ParentState1, setParentState1] = useState(0)
+
+    const [ParentState2, setParentState2] = useState(0)
+
+
+    const BetterChild = useMemo(() => <DemoForMemoChild />, [ParentState2]);
+
     return (
         <div>
-            <h1>DemoForMemo {ParentState}</h1>
-            <button onClick={() => setParentState(ParentState + 1)}>INC Parent</button>
-            <BetterChild />
+            <h1>DemoForMemo </h1>
+            <h1>ParentState1 {ParentState1} <button onClick={() => setParentState1(ParentState1 + 1)}>INC</button></h1>
+            <h1>ParentState2 {ParentState2} <button onClick={() => setParentState2(ParentState2 + 1)}>INC</button></h1>
+            {BetterChild}
+            {/* <DemoForMemoChild/> */}
         </div>
     )
 }
